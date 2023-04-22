3a6dcda8591b8aca724ca9d0cfb9d2ebd6a6ca58
Administrator <Administrator@VS248940>
2023-04-22 17:03:32 +0200

    Projektdateien hinzufügen.

---
+++ CSharpOpenAIDemo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpOpenAIDemo.Startup))]
+namespace CSharpOpenAIDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ CSharpOpenAIDemo/App_Code/classOpenAIAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für classOpenAIAPI
+/// </summary>
+public class classOpenAIAPI
+{
+    public classOpenAIAPI()
+    {
+        //
+        //TODO: Hier Konstruktorlogik hinzufügen
+        //
+    }
+}
